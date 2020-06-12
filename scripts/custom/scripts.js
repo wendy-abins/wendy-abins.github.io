724fd873d1a867e05e18e4f2f94ece8edeb2a33f
Phillip Ashe <ashephillip@gmail.com>
2020-06-12 18:10:39 -0400

    parallax working, need to fix hamburger menu and underneath pictures

---
+++ scripts/custom/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.parallax-window').height($(window).height());
+});
+
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
